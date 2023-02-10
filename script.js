8cdafdf251a954a629b9f3664d0a740a2baee3a8
Mark Conroy <mark@mark.ie>
2023-02-10 12:12:41 +0000

    initial commit of CSS and JS

---
+++ script.js
@@ -0,0 +1,17 @@
+const cssFile = document.querySelector('link[rel="stylesheet"]');
+const cssToggle = document.querySelector('#css');
+const src = cssFile.href;
+const divToCopy = document.querySelector('div');
+const htmlCopyButton = document.querySelector('header button');
+
+cssToggle.addEventListener('click', () => {
+  if (cssToggle.checked) {
+    cssFile.removeAttribute('href');
+  } else {
+    cssFile.setAttribute('href', src);
+  }
+});
+
+htmlCopyButton.addEventListener('click', () => {
+  navigator.clipboard.writeText(divToCopy.innerHTML);
+});
